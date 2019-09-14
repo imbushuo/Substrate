16e638dad5a0cad42f6262f15af207c0db735d6b
Bingxing Wang <i@imbushuo.net>
2019-09-13 22:33:55 -0400

    Project: Initial commit.

---
+++ Substrate.ContentPipeline.Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Substrate.ContentPipeline.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
