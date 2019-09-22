9483a5629de867e71bb7711355cb9f44f6e49d68
Bingxing Wang <i@imbushuo.net>
2019-09-22 04:24:23 -0400

    Add telemetries to Edge service

--- Substrate.Edge/Startup.cs
+++ Substrate.Edge/Startup.cs
@@ -31,6 +31,8 @@ namespace Substrate.Edge
             services.AddSingleton<MediaWikiApiServices>();
             services.AddSingleton<PageRepository>();
             services.AddHostedService<PageCacheUpdater>();
+
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
